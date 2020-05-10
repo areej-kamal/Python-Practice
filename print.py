f04ce37bc8bd32642da1e82717e3eb32afcd0a1b
areej-kamal <59165768+areej-kamal@users.noreply.github.com>
2020-05-10 21:50:43 +0500

    Add files via upload

---
+++ print.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World") # string
+
+
+# In[2]:
+
+
+print(1000) #integer
+
+
+# In[4]:
+
+
+print("My","name","is","Areej!",sep="  ")
+
+
+# In[ ]:
+
+
+
+
