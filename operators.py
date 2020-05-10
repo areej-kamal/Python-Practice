1b56058e1baaa7ead8ed27ece238749c0b46c810
areej-kamal <59165768+areej-kamal@users.noreply.github.com>
2020-05-10 23:22:41 +0500

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# operator {+, -, *, /}
+
+
+# In[2]:
+
+
+print(2+3)
+
+
+# In[3]:
+
+
+a = 10
+b = 11
+c = a + b
+
+
+# In[4]:
+
+
+c
+
+
+# In[5]:
+
+
+value1 = 100
+value2 = 1.99
+value3 = value1 - value2
+
+
+# In[6]:
+
+
+value3
+
+
+# In[7]:
+
+
+type(value3)
+
+
+# In[8]:
+
+
+s1 = 10
+s2 = 10
+s3 = s1 * s2
+print(s3)
+
+
+# In[11]:
+
+
+x = 100
+y = 100
+z = x/y
+print(z)          # python bydefault does flooting division
+z2 = x//y         # integer division ( cuts down the fractional part )
+print(z2)
+
+
+# In[12]:
+
+
+# string + is not sum but concetenation
+a = "areej"
+b = "kamal"
+c = a + b
+print(c)
+
+
+# In[ ]:
+
+
+
+
---
+++ variables.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=10
+
+
+# In[6]:
+
+
+print(a)
+
+
+# In[3]:
+
+
+b="Areej Kamal"
+print(type(b))
+
+
+# In[7]:
+
+
+type(a)
+
+
+# In[8]:
+
+
+c=10.22
+print(type(c))
+
+
+# In[10]:
+
+
+student_name="Areej Kamal"
+father_name="Syed Kamal"
+print(student_name+father_name)
+
+
+# 
+
+# In[11]:
+
+
+num1=100
+num2=200
+sum=num1+num2
+print(sum)
+
+
+# In[ ]:
+
+
+
+
