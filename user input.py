b92c79fc263340a81eaa56700eaf0503c9e515cc
areej-kamal <59165768+areej-kamal@users.noreply.github.com>
2020-05-11 21:22:58 +0500

    Add files via upload

---
+++ user input.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# input always stored as string
+value1 = input("Enter first number: ")
+value2 = input("Enter second number: ")
+value3 = value1 + value2
+
+
+# In[2]:
+
+
+value3
+
+
+# In[3]:
+
+
+value4 = int(value1)             # changing the string input into integer
+value5 = int(value2)
+print(value4+value5)
+
+
+# In[ ]:
+
+
+
+
