327c974b4f866581c79afd4974c5e07a7d3ff795
areej-kamal <59165768+areej-kamal@users.noreply.github.com>
2020-05-28 00:33:47 +0500

    Add files via upload

---
+++ if - else.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+option="dog"
+if option=="cat":
+    print('Yay this is a cat')
+else:
+        print('No this is not a cat')
+
+
+# In[ ]:
+
+
+
+
